58298a615b81bf5170036be43e130408c4a8fea5
Zach Leslie <xaque208@gmail.com>
2024-09-22 15:47:34 +0000

    Testing

--- cmd/pflogger/main.go
+++ cmd/pflogger/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/davecgh/go-spew/spew"
 	"github.com/google/gopacket"
 	_ "github.com/google/gopacket/layers"
@@ -11,19 +14,26 @@ const (
 	// The same default as tcpdump.
 	// defaultSnapLen = 262144
 	defaultSnapLen = 0
+	inf            = "pflog0"
 )
 
 func main() {
-	handle, err := pcap.OpenLive("pflog0", defaultSnapLen, true,
-		pcap.BlockForever)
+	handler := slog.NewTextHandler(os.Stdout, nil)
+	log := slog.New(handler)
+
+	log.Info("capturing", "interface", inf)
+	handle, err := pcap.OpenLive("pflog0", defaultSnapLen, true, pcap.BlockForever)
 	if err != nil {
-		panic(err)
+		log.Error("failed to capture", "interface", inf, "err", err)
+		return
 	}
 	defer handle.Close()
 
-	// if err := handle.SetBPFFilter("port 3030"); err != nil {
-	// 	panic(err)
-	// }
+	log.Info("setting filter")
+	if err := handle.SetBPFFilter(""); err != nil {
+		log.Error("failed to set filter", "err", err)
+		return
+	}
 
 	packets := gopacket.NewPacketSource(handle, handle.LinkType()).Packets()
 	for pkt := range packets {
